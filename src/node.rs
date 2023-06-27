9e21dbe3a6ea377d54ce59350471384b77c675b7
giluis <lupevibogil@gmail.com>
2023-06-27 19:38:12 +0100

    upadted from_disjunct invocation
    Signed-off-by: giluis <lupevibogil@gmail.com>

--- src/node.rs
+++ src/node.rs
@@ -11,9 +11,10 @@ fn get_token_type_alias() -> syn::Ident {
 fn disjunct_all(variants: Punctuated<Variant, Comma>, node_ident: &syn::Ident) -> TokenStream {
     let consumption_statements = variants.iter().map(|v| disjunct(v, node_ident));
     let err_vars = variants.iter().map(|v| v.ident.as_error_variable());
+    let type_name = node_ident.to_string();
     quote! {
         #(#consumption_statements)*
-        Err(ParseError::from_disjunct_errors::<#node_ident>(iter.current, vec![#(#err_vars,)*]))
+        Err(ParseError::from_disjunct_errors::<#node_ident>(iter.current, vec![#(#err_vars,)*], #type_name))
     }
 }
 
